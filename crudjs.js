98b4355addfa66d8d07ff20170c65202c9164d43
MuhWijdanandiPratama <pratamwijdanandi@gmail.com>
2022-08-10 18:36:33 +0700

    first commit

---
+++ crudjs.js
@@ -0,0 +1,39 @@
+var data = [
+    "Pensil",
+    "Buku Tulis",
+    "Pulpen",
+    "Penghapus",
+    "Stipo"
+];
+
+
+function tampil() {
+    var tabel = document.getElementById("tabel");
+    tabel.innerHTML = "<tr><th>No</th><th>Nama-Barang</th><th>Action</th></tr>";
+    for (let i = 0; i < data.length; i++) {
+        var btnEdit = "<button class='btn-edit' href='#' onclick='edit(" + i + ")'>Edit</button>";
+        var btnHapus = "<button class='btn-hapus' href='#' onclick='hapus(" + i + ")'>Hapus</button>";
+        j = i + 1;
+        tabel.innerHTML += "<tr><td>" + j + "</td><td>" + data[i] + "</td><td>" + btnEdit + " " + btnHapus + "</tr>";
+    }
+}
+
+function tambah() {
+    var input = document.querySelector("input[name=Nama-Barang]");
+    data.push(input.value);
+    tampil();
+    input.value = "";
+}
+
+function edit(id) {
+    var baru = prompt("Edit", data[id]);
+    data[id] = baru;
+    tampil();
+}
+
+function hapus(id) {
+    data.pop(id);
+    tampil();
+}
+
+tampil();
